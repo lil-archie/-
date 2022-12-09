75727392dfc43ea270d22f0b845d198fee9b4c71
lil-archie <108964143+lil-archie@users.noreply.github.com>
2022-12-09 22:53:28 +0800

    Add files via upload

---
+++ test_12_9/test.c
@@ -0,0 +1,60 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//打印三角形的符号
+//int main()
+//{
+//    char sign = 0;
+//    scanf("%c", &sign);
+//    int i = 0;
+//    for (i = 0; i < 5; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < 4 - i; j++)
+//        {
+//            printf(" ");
+//        }
+//        for (j = 0; j <= i; j++)
+//        {
+//            printf("%c ", sign);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+//除数与余数的打印
+//#include <stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    printf("%d %d", a / b, a % b);
+//}
+//int main()
+//{
+//	/*int a = 65;
+//	printf("%c", a);*/
+//	/*char a = 73;
+//	printf("%c", a);*/
+//}
+// asic码表的输出
+//int main()
+//{#include <stdio.h>
+//
+//	printf("%c%c%c%c%c%c%c%c%c%c%c%c", 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33);
+//}
+//#include <stdio.h>
+//int main()
+//{
+//    char arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 , 33 };
+//    int i = 0;
+//    //计算数组的元素个数
+//    int sz = sizeof(arr) / sizeof(arr[0]);
+//    //循环遍历输出每一个元素
+//    for (i = 0; i < sz; i++)
+//    {
+//        //%c 控制打印字符
+//        printf("%c", arr[i]);//每个字符不要隔开
+//    }
+//    return 0;
+//}
