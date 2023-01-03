1f06e462c4df92b28b3862fc3e36f8a83debf926
lil-archie <108964143+lil-archie@users.noreply.github.com>
2023-01-03 20:56:41 +0800

    Add files via upload

---
+++ test_1_3/add.c
@@ -0,0 +1,18 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	unsigned char a = 200;
+//	unsigned char b = 100;
+//	unsigned char c = 0;
+//	c = a + b;
+//	printf(" %d %d", a + b, c);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	printf("%d", a);
+	return 0;
+}
