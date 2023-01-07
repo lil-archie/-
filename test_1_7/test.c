410f5be050fbd58f3eb55e38abd876be2cc3d192
lil-archie <108964143+lil-archie@users.noreply.github.com>
2023-01-07 21:20:47 +0800

    Add files via upload

---
+++ test_1_7/test.c
@@ -0,0 +1,31 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//void print_arr(int(*arr)[5], int row, int col)
+//{
+//	for (int i = 0; i < row; i++)
+//	{
+//		for (int j = 0; j < col; j++)
+//		{
+//			printf("%d", arr[i][j]);
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { 1,2,3,4,5,2,3,4,5,6,3,4,5,6,7 };
+//	print_arr(arr, 3, 5);
+//	return 0;
+//}
+//typedef unsigned int uint;
+//    typedef void(*pf_t)(int);
+//	pf_t signal(int, pf_t);
+//void (*signal(int, void(*)(int)))(int);
+//	//该代码是一次函数的声明
+//	//声明的函数名字叫signal
+//	//signal函数的参数有2个，第一个是int类型，第二个是函数指针类型，该函数指针能够指向的那个函数的参数是int
+//	//返回类型是void
+//	//signal函数的返回类型是一个函数指针，该函数指针能够指向的那个函数的参数是int,返回类型是void
+//	// 
+//	//void (*)(int) signal(int, void(*)(int));
+//
+//	return 0;
