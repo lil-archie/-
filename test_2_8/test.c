4cdba590afe9332e0491cfe976797826d88986e5
lil-archie <108964143+lil-archie@users.noreply.github.com>
2023-02-08 21:26:36 +0800

    Add files via upload

---
+++ test_2_8/test.c
@@ -0,0 +1,183 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//
+//int main()
+//{
+//	printf("hehe\n");
+//	return 0;
+//}
+
+//ⲿ
+//extern int Add(int x, int y);
+//extern int g_val;
+//
+//int main()
+//{
+//	printf("%d\n", g_val);
+//
+//	int x = 0;
+//	int y = 0;
+//	scanf("%d %d", &x, &y);
+//	int ret = Add(x, y);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+
+//
+//int main()
+//{
+//	printf("%s\n", __FILE__);
+//	printf("%d\n", __LINE__);
+//	printf("%s\n", __DATE__);
+//	printf("%s\n", __TIME__);
+//	//printf("%d\n", __STDC__);
+//	printf("%s\n", __FUNCTION__);
+//	return 0;
+//}
+
+
+//#define do_forever for(;;)
+//
+//int main()
+//{
+//	do_forever;
+//	return 0;
+//}
+
+//switch case
+
+//#define CASE break;case
+//
+//int main()
+//{
+//	int n = 0;
+//	switch (n)
+//	{
+//	case 1:
+//	CASE 2:
+//	CASE 3:
+//	CASE 4:
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int n = 0;
+//	switch(n)
+//	{ 
+//	case 1:
+//		break;
+//	case 2:
+//		break;
+//	}
+//	return 0;
+//}
+//
+//#define MAX 1000;
+//
+//int main()
+//{
+//	int m = MAX;
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+
+
+//#define MAX 1000;
+//
+//int main()
+//{
+//	int m = 0;
+//	if (m >= 0)
+//		m = MAX;
+//	else
+//		m = -1;
+//
+//	printf("%d\n", m);
+//
+//	return 0;
+//}
+
+
+//#define SQUARE(X) ((X)*(X))
+//
+//int main()
+//{
+//	printf("%d\n", SQUARE(5+1));
+//	//printf("%lf\n", SQUARE(5.0));
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//#define DOUBLE(X) ((X)+(X))
+//
+//int main()
+//{
+//	printf("%d\n", 10*DOUBLE(3));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("hello bit\n");
+//	printf("hello " "bit\n");
+//
+//	return 0;
+//}
+
+//#define PRINT(format, x) printf("the value of "#x" is "format"\n", x)
+//
+//int main()
+//{
+//	int a = 10;
+//	//printf("the value of a is %d\n", a);
+//	PRINT("%d", a);
+//	//printf("the value of ""a"" is ""%d""\n", x)
+//	int b = 20;
+//	//printf("the value of b is %d\n", b);
+//	PRINT("%d", b);
+//
+//	float f = 3.14f;
+//	PRINT("%f", f);
+//	//printf("the value of ""f"" is ""%f""\n", x)
+//	return 0;
+//}
+
+//#define CAT(x,y) x##y
+////Class##109
+////Class109
+//int main()
+//{
+//	int Class109 = 2023;
+//	printf("%d\n", CAT(Class, 109));
+//
+//	return 0;
+//}
+
+
+//
+//#define _CRT_SECURE_NO_WARNINGS 1
+////
+////int Add(int x, int y)
+////{
+////	return x + y;
+////}
+////
+////int g_val = 2023;
+////
+
+
+
+-
+
